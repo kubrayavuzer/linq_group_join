98c1a9a56df16d3ed0fd0983df105b34bf192c0b
Kübra Yavuzer <kubrayavuzeer@gmail.com>
2024-09-05 03:25:55 +0300

    Proje dosyası ekle.

---
+++ linq_group_join/Program.cs
@@ -0,0 +1,47 @@
+﻿using linq_group_join;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<students> student = new List<students>
+        {
+            new students {StudentId = 1, StudentName = "Ali", ClassId = 1 },
+            new students {StudentId = 2, StudentName = "Ayşe", ClassId = 2 },
+            new students {StudentId = 3, StudentName = "Mehmet", ClassId = 1},
+            new students { StudentId = 4, StudentName = "Fatma", ClassId = 3},
+            new students {StudentId = 5, StudentName = "Ahmet", ClassId = 2},
+        };
+
+        List<Class> classes = new List<Class>
+        {
+
+            new Class {ClassId = 1, ClassName = "Matematik"},
+            new Class {ClassId =2, ClassName = "Türkçe"},
+            new Class {ClassId = 3, ClassName = "Kimya"},
+
+        };
+
+        var groupJoin = from cls in classes
+                        join std in student
+                        on cls.ClassId equals std.StudentId into group_Join
+                        select new
+                        {
+                            ClassName = cls.ClassName,
+                            student = group_Join
+
+                        };
+
+        foreach (var group in groupJoin)
+        {
+            Console.WriteLine($"Sınıf: {group.ClassName}");
+
+            foreach (var students in group.student)
+            {
+
+                Console.WriteLine($"Öğrenci: {students.StudentName}");
+            }
+
+
+        }
+    }
+}
---
+++ linq_group_join/classes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_group_join
+{
+    public class Class
+    { 
+
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+
+    }
+}
